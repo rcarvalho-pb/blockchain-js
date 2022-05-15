50b15358093cd93b6983fd6077f64588d6b4d3d8
Ramon Carvalho <rcarvalho.pb@gmail.com>
2022-05-14 23:26:31 -0300

    Cleaning documents

--- teste.js
+++
@@ -1,9 +0,0 @@
-const Block = require("./block");
-const Blockchain = require("./blockchain");
-
-const block = Block.mineBlock(Block.genesis(), "Alow");
-
-const chain = [Block.genesis(), block];
-
-if(JSON.stringify(chain[0] === JSON.stringify(Block.genesis()))) console.log("oka")
-else console.log("Wrong!!")
